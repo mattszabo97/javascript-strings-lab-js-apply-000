398d953d582c7841663595e157ff716f4ef47457
Matthew Szabo <mattmszabo@gmail.com>
2019-08-14 17:55:50 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -2,12 +2,14 @@
 
 it('defines `greeting`', function() {
   expect(greeting).toEqual("Hello, everybody!")
-})
+}) ("Hello, everybody")
 
 it('concatenates strings to greet a special guest in `greetSpecialGuest`', function() {
   expect(greetSpecialGuest).toEqual(`Hello, ${specialGuest}!`)
-})
+}) var greetSpecialGues
+
 
 it('interpolates a string in `conversation`', function() {
   expect(conversation).toEqual(`Let's talk about ${topic}.`)
-})
+})var ${topic}
+
